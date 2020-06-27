8e305d1bd2dbb354174a7497c083700209c08c49
Jaren Glenn <jarenglenn@gmail.com>
2020-06-27 02:10:27 -0600

    implemented proper naming of the pdf based on title and author

--- src/js/content.js
+++ src/js/content.js
@@ -16,10 +16,22 @@ function createPDF(sheetClassName) {
 	});
 
 	$( sheetClassName ).each(function() {
-		sheetPDF.addImage($(this)[0], 'PNG', 0, 0, 8.5, 11);
+		sheetPDF.addImage($(this)[0], 'PNG', 0, 0, 8.5, 11); // Format image for letter paper
 		sheetPDF.addPage("letter");
 	});
 	
-	sheetPDF.deletePage(sheetPDF.internal.getNumberOfPages());
-	sheetPDF.save('sheets.pdf');
+	sheetPDF.deletePage(sheetPDF.internal.getNumberOfPages()); // Remove the last empty page
+
+	var title = $('.rdHmC').text();
+	var author = $('._3qfU_._3ysVT._1Us9e._3HJAX._3hjK9._15kzJ').children( $('._3R0py')).text();
+
+	if (author !== "" && title !== "") {
+		sheetPDF.save(title + " - " + author + ".pdf");
+	} else if (title !== "") {
+		sheetPDF.save(title + ".pdf");
+	} else {
+		sheetPDF.save("sheetmusic.pdf");
+	}
+
+	sheetPDF.output('dataurlnewwindow');
 };
